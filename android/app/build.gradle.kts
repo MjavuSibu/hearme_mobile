6d3e213030ea06ce259d579698959117b3462e8b
Sibu Mjavu <mjavusibulele@gmail.com>
2026-04-07 14:14:14 +0200

    Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+    isCoreLibraryDesugaringEnabled = true
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
@@ -41,4 +42,11 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation("androidx.window:window:1.0.0")
+    implementation("androidx.window:window-java:1.0.0")
+    
 }
